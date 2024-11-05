1f2f947f6685b0ca31fb3b17dfc8bdce7b0f51b4
nonssoo <124942405+nonssoo@users.noreply.github.com>
2024-11-05 10:57:52 +0200

    Add files via upload

---
+++ process_foem.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$category = $_POST['category'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO contact_form (name, email, category, subject, message)
+VALUES ('$name', '$email', '$category', '$subject', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
